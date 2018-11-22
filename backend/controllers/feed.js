d3a1e0d5279c09e599c7a496e6ac209c482e57e5
Marek Dano <mk.dano@gmail.com>
2018-11-22 08:25:32 +0000

    Add a method of creating a post in feed.

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -3,3 +3,13 @@ exports.getPosts = (req, res, next) => {
 		posts: [{ title: 'My First Post', content: 'This is my first post.' }]
 	});
 };
+
+exports.createPost = (req, res, next) => {
+	const title = req.body.title;
+	const content = req.body.content;
+	// create post in db
+	res.status(201).json({
+		message: 'Post created successfully!',
+		post: { id: new Date().toISOString(), title, content }
+	});
+};
