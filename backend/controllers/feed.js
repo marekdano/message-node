9e4d95db5cef5577a419efe7c82d035aea2dda42
Marek Dano <mk.dano@gmail.com>
2018-11-29 10:50:58 +0000

    Add logic to save a new post to the database.

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator/check');
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
 	res.json({
@@ -29,17 +30,24 @@ exports.createPost = (req, res, next) => {
 	}
 	const title = req.body.title;
 	const content = req.body.content;
-	// create post in db
-	res.status(201).json({
-		message: 'Post created successfully!',
-		post: { 
-			_id: new Date().toISOString(), 
-			title: title, 
-			content: content,
-			creator: {
-				name: 'Marek'
-			},
-			createdAt: new Date()
-		}
+	const post = new Post({
+		title: title, 
+		content: content,
+		imageUrl: 'images/sample.jpg',
+		creator: {
+			name: 'Marek'
+		},
 	});
+	// create post in db
+	post
+		.save()
+		.then(result => {
+			res.status(201).json({
+				message: 'Post created successfully!',
+				post: result
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
 };
