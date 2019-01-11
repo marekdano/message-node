a27e779b622ee34a0d41920318b19b3c75b5c75b
Marek Dano <mk.dano@gmail.com>
2019-01-11 22:23:25 +0000

    Connect posts and users in the backend.

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 const errorHandlingInPromiseCatch = (err, next) => {
 	if (!err.statusCode) {
@@ -54,21 +55,29 @@ exports.createPost = (req, res, next) => {
 	const title = req.body.title;
 	const content = req.body.content;
 	const imageUrl = req.file.path;
+	let creator;
 	const post = new Post({
 		title: title, 
 		content,
 		imageUrl,
-		creator: {
-			name: 'Marek'
-		},
+		creator: req.userId,
 	});
 	// create post in db
 	post
 		.save()
 		.then(result => {
+			return User.findById(req.userId);
+		})
+		.then(user => {
+			creator = user;
+			user.posts.push(post);
+			return user.save();
+		})
+		.then(result => {
 			res.status(201).json({
 				message: 'Post created successfully!',
-				post: result
+				post: post,
+				creator: { _id: creator._id, name: creator.name },
 			});
 		})
 		.catch(err => {
