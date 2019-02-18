653dc4cc32cfe2d01f73bc3d6e1c5d1412b0bfea
Marek Dano <mk.dano@gmail.com>
2019-02-18 21:22:10 +0000

    Rename validation.spec.js file. Create filePicker test file.

---
+++ frontend/src/components/Form/Input/__tests__/filePicker.js
@@ -0,0 +1,14 @@
+import { toHaveAttribute, toHaveTextContent } from 'jest-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FilePicker from '../FilePicker';
+
+expect.extend({toHaveAttribute, toHaveTextContent});
+
+test('renders a number input with a label "File"', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<FilePicker id={1} label={'File'}/>, div);
+  expect(div.querySelector('input')).toHaveAttribute('type', 'file');
+  // expect(div.querySelector('label').textContent).toBe('File');
+  expect(div.querySelector('label')).toHaveTextContent('File');
+});
