8b96a51939ded4784fbfbe7be897afafb6c7cb4f
Marek Dano <mk.dano@gmail.com>
2019-02-19 21:02:19 +0000

    Modify test in the filePicker test file with 'queries' from dom-testing-library.

--- frontend/src/components/Form/Input/__tests__/filePicker.js
+++ frontend/src/components/Form/Input/__tests__/filePicker.js
@@ -1,11 +1,15 @@
 import 'jest-dom/extend-expect';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {getQueriesForElement} from 'dom-testing-library';
 import FilePicker from '../FilePicker';
 
 test('renders a number input with a label "File"', () => {
   const div = document.createElement('div');
   ReactDOM.render(<FilePicker id={1} label={'File'}/>, div);
-  expect(div.querySelector('input')).toHaveAttribute('type', 'file');
-  expect(div.querySelector('label')).toHaveTextContent('File');
+  // expect(div.querySelector('input')).toHaveAttribute('type', 'file');
+  // expect(div.querySelector('label')).toHaveTextContent('File');
+  const {getByLabelText} = getQueriesForElement(div);
+  const input = getByLabelText(/file/i);
+  expect(input).toHaveAttribute('type', 'file');
 });
